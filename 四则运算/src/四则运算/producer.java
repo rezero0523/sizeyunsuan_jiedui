e845d081b0724e3d45d9a5583aa0f8e97b7c878f
lin7940 <61583943+lin7940@users.noreply.github.com>
2020-03-25 15:34:19 +0800

    对一些问题进行修改

--- 四则运算/src/四则运算/producer.java
+++ 四则运算/src/四则运算/producer.java
@@ -1,4 +1,4 @@
-package ËÄÔòÔËËã;
+package å››åˆ™è¿ç®—;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -6,40 +6,49 @@ import java.util.Scanner;
 public class producer {
 public static void main(String arg[])
 {
-	int answer;//´æ´¢´ğ°¸
+	int answer;//å­˜å‚¨ç­”æ¡ˆ
+	int success;//å‡ºé¢˜åˆ¤æ–­æ ‡å¿—
 	Scanner scanner=new Scanner(System.in);
 	Random random=new Random();
-	System.out.println("ÇëÊäÈëÒª³öµÄÌâÄ¿¸öÊı£º");
+	System.out.println("è¯·è¾“å…¥è¦å‡ºçš„é¢˜ç›®ä¸ªæ•°ï¼š");
 	int p_number=scanner.nextInt();
 	for(int i=0;i<p_number;i++)
 	{
 		int r=random.nextInt(4)+1;
 		switch (r) {
 		case 1:
+			do{
 			int left1=random.nextInt(101);
 			int right1=random.nextInt(101);
 			System.out.println(left1+"+"+right1+"=");
 			answer=left1+right1;
+			}while(answer>100);
 			break;
 		case 2:
+			do{
 			int left2=random.nextInt(101);
 			int right2=random.nextInt(101);
 			System.out.println(left2+"-"+right2+"=");
-			answer=left2+right2;
+			answer=left2-right2;
+			}while(answer<=0);
 			break;
 		case 3:
+			do{
 			int left3=random.nextInt(101);
 			int right3=random.nextInt(101);
 			System.out.println(left3+"*"+right3+"=");
-			answer=left3+right3;
+			answer=left3*right3;
+			}while(answer>100);
 			break;
 		case 4:
+			do{
 			int left4=random.nextInt(101);
 			int right4=random.nextInt(101);
 			System.out.println(left4+"/"+right4+"=");
-			answer=left4+right4;
+			success=left4%right4;
+			answer=left4/right4;
+			}while(success!=0);
 			break;
-			
 		}
 		
 	}
