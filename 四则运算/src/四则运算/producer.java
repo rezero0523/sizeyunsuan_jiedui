9288fae64a023727af6a04a67a6a1188e517d111
lin7940 <61583943+lin7940@users.noreply.github.com>
2020-03-25 20:36:06 +0800

    四则运算第二次修改

--- 四则运算/src/四则运算/producer.java
+++ 四则运算/src/四则运算/producer.java
@@ -8,6 +8,7 @@ public static void main(String arg[])
 {
 	int answer;//存储答案
 	int success;//出题判断标志
+	@SuppressWarnings("resource")
 	Scanner scanner=new Scanner(System.in);
 	Random random=new Random();
 	System.out.println("请输入要出的题目个数：");
@@ -23,6 +24,12 @@ public static void main(String arg[])
 			System.out.println(left1+"+"+right1+"=");
 			answer=left1+right1;
 			}while(answer>100);
+			System.out.println("请回答：");
+			int u_answer1=scanner.nextInt();
+			if(u_answer1==answer)
+			System.out.println("答案正确");
+			else 
+			System.out.println("答案错误");
 			break;
 		case 2:
 			do{
@@ -31,6 +38,12 @@ public static void main(String arg[])
 			System.out.println(left2+"-"+right2+"=");
 			answer=left2-right2;
 			}while(answer<=0);
+			System.out.println("请回答：");
+			int u_answer2=scanner.nextInt();
+			if(u_answer2==answer)
+			System.out.println("答案正确");
+			else 
+			System.out.println("答案错误");
 			break;
 		case 3:
 			do{
@@ -39,6 +52,12 @@ public static void main(String arg[])
 			System.out.println(left3+"*"+right3+"=");
 			answer=left3*right3;
 			}while(answer>100);
+			System.out.println("请回答：");
+			int u_answer3=scanner.nextInt();
+			if(u_answer3==answer)
+			System.out.println("答案正确");
+			else 
+			System.out.println("答案错误");
 			break;
 		case 4:
 			do{
@@ -48,6 +67,12 @@ public static void main(String arg[])
 			success=left4%right4;
 			answer=left4/right4;
 			}while(success!=0);
+			System.out.println("请回答：");
+			int u_answer4=scanner.nextInt();
+			if(u_answer4==answer)
+			System.out.println("答案正确");
+			else 
+			System.out.println("答案错误");
 			break;
 		}
 		
