334fea6160a91503cdc92d64fac26f6d3e4405be
恋色 <2981347284@qq.com>
2020-03-26 13:25:03 +0800

    程序可运行修正

--- 四则运算/src/四则运算/producer.java
+++ 四则运算/src/四则运算/producer.java
@@ -7,6 +7,11 @@ public class producer {
 public static void main(String arg[])
 {
 	int answer;//存储答案
+	int success;//出题判断标志
+	int t_answer=0;//答对的题目总数
+	int left;
+	int right;
+	@SuppressWarnings("resource")
 	Scanner scanner=new Scanner(System.in);
 	Random random=new Random();
 	System.out.println("请输入要出的题目个数：");
@@ -16,32 +21,85 @@ public static void main(String arg[])
 		int r=random.nextInt(4)+1;
 		switch (r) {
 		case 1:
+			do{
 			int left1=random.nextInt(101);
 			int right1=random.nextInt(101);
-			System.out.println(left1+"+"+right1+"=");
-			answer=left1+right1;
+			left=left1;
+			right=right1;
+			answer=left+right;
+			}while(answer>100);
+			System.out.println(left+"+"+right+"=");
+			System.out.println("请回答：");
+			int u_answer1=scanner.nextInt();
+			if(u_answer1==answer)
+			{
+				System.out.println("答案正确");
+				t_answer++;
+			}
+			else 
+			System.out.println("答案错误");
 			break;
 		case 2:
+			do{
 			int left2=random.nextInt(101);
 			int right2=random.nextInt(101);
-			System.out.println(left2+"-"+right2+"=");
-			answer=left2+right2;
+			left=left2;
+			right=right2;
+			answer=left-right;
+			}while(answer<=0);
+			System.out.println(left+"-"+right+"=");
+			System.out.println("请回答：");
+			int u_answer2=scanner.nextInt();
+			if(u_answer2==answer)
+			{
+			System.out.println("答案正确");
+			t_answer++;
+			}
+			else 
+			System.out.println("答案错误");
 			break;
 		case 3:
+			do{
 			int left3=random.nextInt(101);
 			int right3=random.nextInt(101);
-			System.out.println(left3+"*"+right3+"=");
-			answer=left3+right3;
+			left=left3;
+			right=right3;
+			answer=left*right;
+			}while(answer>100);
+			System.out.println(left+"*"+right+"=");
+			System.out.println("请回答：");
+			int u_answer3=scanner.nextInt();
+			if(u_answer3==answer)
+			{
+			System.out.println("答案正确");
+			t_answer++;
+			}
+			else 
+			System.out.println("答案错误");
 			break;
 		case 4:
+			do{
 			int left4=random.nextInt(101);
 			int right4=random.nextInt(101);
-			System.out.println(left4+"/"+right4+"=");
-			answer=left4+right4;
+			success=left4%right4;
+			left=left4;
+			right=right4;
+			answer=left/right4;
+			}while(success!=0);
+			System.out.println(left+"/"+right+"=");
+			System.out.println("请回答：");
+			int u_answer4=scanner.nextInt();
+			if(u_answer4==answer)
+			{
+			System.out.println("答案正确");
+			t_answer++;
+			}
+			else 
+			System.out.println("答案错误");
 			break;
-			
 		}
-		
 	}
+	System.out.println("您的得分是"+t_answer*100/p_number);
+	t_answer=0;
 }
 }
